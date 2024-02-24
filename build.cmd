bedf14db922deeadf5465e8e3aac5a04fd1f158e
7aGiven <fredrickareichert@gmail.com>
2024-02-24 10:09:40 +0800

    build

--- build.cmd
+++
@@ -1,3 +0,0 @@
-cl.exe /c /I src src\cJSON.c src\phigros.c src\score.cpp
-lib.exe /OUT:phigros.lib src\zip.lib cJSON.obj phigros.obj score.obj
-del cJSON.obj phigros.obj score.obj
--- build.sh
+++
@@ -1,5 +0,0 @@
-g++ -c -fPIC src/score.cpp
-gcc -c -fPIC -I src src/cJSON.c src/phigros.c
-cp src/libzip.a phigros.a
-ar r phigros.a cJSON.o phigros.o score.o
-rm cJSON.o phigros.o score.o
